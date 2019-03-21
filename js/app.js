cb60f263f0c45c75c611ad556d8838f95a48ea6e
Sky Seulki <admin@Skys-MacBook-Pro.local>
2019-03-21 16:49:46 -0700

    commit 1 - verbal questions

---
+++ js/app.js
@@ -0,0 +1,10 @@
+console.log("Week1-Thursday-hw");
+
+// 1. Verbal Q's
+// 1. Parameters are variables that defines the function's declaration.
+//     => provides extra information for a function
+//     Whereas the arguments are the actual values passed to the function when the function is called.
+// 2. A return log is a call to action statement. 
+//     It will cause the function that contains it to immediately end the line after it's run.
+//     A console.log outputs a message to the web console.
+// 3. "Return statements allow us to "spit out" or "hand back" a value to the line where we called the function"
